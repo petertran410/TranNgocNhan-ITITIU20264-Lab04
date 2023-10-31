642b2f504c6bedee298b72aed1d349490dcb3a81
petertran410 <nhantran4102002@gmail.com>
2023-10-31 23:24:51 +0700

    1

---
+++ basic-android-kotlin-compose-training-reply-app-starter/app/src/main/java/com/example/reply/MainActivity.kt
@@ -0,0 +1,46 @@
+
+package com.example.reply
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.Surface
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.reply.ui.ReplyApp
+import com.example.reply.ui.theme.ReplyTheme
+
+class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            ReplyTheme {
+                Surface {
+                    val windowSize = calculateWindowSizeClass(this)
+
+                    ReplyApp(
+                        windowSize = windowSize.widthSizeClass,
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ReplyAppCompactPreview() {
+    ReplyTheme {
+        Surface {
+            ReplyApp(
+                windowSize = WindowWidthSizeClass.Compact,
+            )
+        }
+    }
+}
